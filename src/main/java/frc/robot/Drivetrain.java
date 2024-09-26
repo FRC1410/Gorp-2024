103dd54d56dab1c18c1c618bd9d90b85cbef2884
Samuel Rojas <samprogram7@gmail.com>
2024-09-25 18:23:31 -0600

    resolve it please

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,29 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import static frc.robot.Constants.*;
+
+public class Drivetrain {
+
+    private final WPI_TalonSRX leftFrontMotor = new WPI_TalonSRX(LEFT_FRONT_ID);
+    private final WPI_TalonSRX leftBackMotor = new WPI_TalonSRX(LEFT_BACK_ID);
+    private final WPI_TalonSRX rightBackMotor = new WPI_TalonSRX(RIGHT_BACK_ID);
+    private final WPI_TalonSRX rightFrontMotor = new WPI_TalonSRX(RIGHT_FRONT_ID);
+
+    public DifferentialDrive differentialDrive = new DifferentialDrive(leftFrontMotor, rightFrontMotor);
+    public DifferentialDrive getDifferentialDrive() {return differentialDrive;}
+
+
+    public Drivetrain(){
+        leftBackMotor.follow(leftFrontMotor);
+        rightBackMotor.follow(rightBackMotor);
+    }
+
+    public void tankDrive(double leftStickY, double rightStickX, double leftTrigger, double rightTrigger) {
+        getDifferentialDrive().tankDrive(leftStickY, 0.0 - rightStickX);
+
+
+    }
+    }
