7e64a750e4dcdb1b4dc72da0de661007daa85f7d
Mexican1oreo <93799117+Mexican1oreo@users.noreply.github.com>
2024-09-30 12:34:20 -0600

    Revert "Drivetrain"

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,12 +1,3 @@
 package frc.robot;
 
-public final class Constants {
-
-    public static final int DRIVER_CONTROLLER = 0;
-
-    public final static int LEFT_FRONT_ID = 1;
-    public final static int LEFT_BACK_ID = 2;
-    public final static int RIGHT_FRONT_ID = 3;
-    public final static int RIGHT_BACK_ID = 4;
-
-}
+public final class Constants {}
